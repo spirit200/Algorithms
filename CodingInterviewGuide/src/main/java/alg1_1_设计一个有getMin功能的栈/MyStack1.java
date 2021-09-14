8c50b6c303e56ea51280d6c17585b616d127e6cb
spirit200 <wkx2021@foxmail.com>
2021-09-14 21:56:25 +0800

    添加设计一个有getMin功能栈的第二种解法

--- CodingInterviewGuide/src/main/java/alg1_1_设计一个有getMin功能的栈/MyStack1.java
+++ CodingInterviewGuide/src/main/java/alg1_1_设计一个有getMin功能的栈/MyStack1.java
@@ -1,11 +1,31 @@
-package alg1_1_设计一个有getMin功能的栈;
-
-/**
- * @Author Wangkx
- * @Date 2021/9/3 11:02
- * @Version 1.0
- * @Description:
- */
-public class MyStack1 {
-    
-}
+//
+//package alg1_1_设计一个有getMin功能的栈;
+//
+//import java.util.Stack;
+//
+///**
+// * @Author Wangkx
+// * @Date 2021/9/3 11:02
+// * @Version 1.0
+// * @Description:
+// */
+//public class MyStack1 {
+//    public static void main(String[] args) {
+//        Solution solution = new MyStack1().new Solution();
+//        solution.push(5);
+//    }
+//
+//    class Solution {
+//        private Stack<Integer> stackData;
+//        private Stack<Integer> stackMin;
+//
+//        public Solution() {
+//            this.stackData = new Stack<Integer>();
+//            this.stackMin = new Stack<Integer>();
+//        }
+//
+//        public void push(int newNum) {
+//
+//        }
+//    }
+//}
---
+++ CodingInterviewGuide/src/main/java/alg1_1_设计一个有getMin功能的栈/Solution.java
@@ -0,0 +1,101 @@
+package alg1_1_设计一个有getMin功能的栈;
+
+import java.util.Stack;
+
+/**
+ * @Author Wangkx
+ * @Date 2021/9/3 17:27
+ * @Version 1.0
+ * @Description:
+ */
+public class Solution {
+    public static void main(String[] args) {
+        MyStack2 mystack = new Solution().new MyStack2();
+        mystack.push(3);
+        mystack.push(2);
+        mystack.push(1);
+        System.out.println(mystack.getMin());
+        mystack.pop();
+        int res = mystack.getMin();
+        System.out.println(res);
+    }
+
+    class MyStack {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
+
+        public MyStack() {
+            this.stackData = new Stack<Integer>();
+            this.stackMin = new Stack<Integer>();
+        }
+
+        public void push(int newNum) {
+            if (this.stackMin.isEmpty()) {
+                this.stackMin.push(newNum);
+            } else if (newNum <= this.getmin()) {
+                this.stackMin.push(newNum);
+            }
+            this.stackData.push(newNum);
+        }
+
+        public int pop() {
+            if (this.stackData.isEmpty()) {
+                throw new RuntimeException("Your stack is empty.");
+            }
+            int value = this.stackData.pop();
+            if (value == this.getmin()) {
+                this.stackMin.pop();
+            }
+            return value;
+        }
+
+        public int getmin() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("Your stack is empty.");
+            }
+            return this.stackMin.peek();
+        }
+    }
+
+    class MyStack2 {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
+
+        public MyStack2() {
+            this.stackData = new Stack<>();
+            this.stackMin = new Stack<>();
+        }
+
+        public int getMin() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("空");
+            } else {
+                return this.stackMin.peek();
+            }
+        }
+
+        public void push(int newNum) {
+            if (this.stackMin.isEmpty()) {
+                this.stackMin.push(newNum);
+            } else {//stackMin不为空
+                int min = this.stackMin.peek();
+                if (newNum <= min) {
+                    this.stackMin.push(newNum);
+                } else {
+                    this.stackMin.push(min);
+                }
+            }
+            this.stackData.push(newNum);
+        }
+
+        public int pop() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("空");
+            }
+            this.stackMin.pop();
+            return this.stackData.pop();
+        }
+    }
+}
+
+
